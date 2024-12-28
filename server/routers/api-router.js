d0a00cf6337291ece7ffdd2a7cdf8ed876908313
alirezashirzadath <alirezashirzadath@gmail.com>
2024-12-28 11:50:04 +0330

    fix some bugs

--- server/routers/api-router.js
+++ server/routers/api-router.js
@@ -269,7 +269,7 @@ router.post("/api/add-monitor", async (request, response) => {
     let monitor = R.dispense("monitor");
     monitor.url = "https://google.com";
     monitor.name = "Google";
-    R.store(monitor);
+    await R.store(monitor);
     return response.json({
         success: "Ok",
     });
